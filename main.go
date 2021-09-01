4f820a1f93aeb5c13c28b1275baaf17841fa5aa9
Jean Carlos Molossi <jean.molossi@catho.com>
2021-09-01 18:49:50 -0300

    refactor: adicionada instancia de worker e executando

--- main.go
+++ main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
+	"log"
 	"up-planilhas-go/core"
 )
 
 func main() {
-	core.WorkerRunner()
+	done := make(chan bool)
+
+	db, err := core.NewDbConnection()
+	if err != nil {
+		log.Fatalf("Error on DB connection")
+	}
+
+	worker := core.NewWorker(db)
+	worker.Workers = 175
+	worker.WorkerRunner(done)
 }
