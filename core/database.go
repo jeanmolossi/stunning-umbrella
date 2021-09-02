18dc589236d82f5d193b30b041a85349eb91c780
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2021-09-02 00:30:41 -0300

    refactor: adicionado logmode e limpeza de banco inicial

--- core/database.go
+++ core/database.go
@@ -1,21 +1,34 @@
 package core
 
 import (
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"os"
+	"strconv"
 )
 
 func NewDbConnection() (*gorm.DB, error) {
 	dsn := "root:root@tcp(up_db:3306)/logger_base"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open("mysql", dsn)
 
 	if err != nil {
 		return nil, err
 	}
 
-	err = db.AutoMigrate(&Log{})
+	shouldEnableLogs, err := strconv.ParseBool(os.Getenv("ENABLE_DB_LOGS"))
 	if err != nil {
 		return nil, err
+	}
+
+	db.AutoMigrate(&Log{})
+	db.LogMode(shouldEnableLogs)
+	if err != nil {
+		return nil, err
+	}
+
+	shouldCleanDb, err := strconv.ParseBool(os.Getenv("SHOULD_CLEAN_DB"))
+	if shouldCleanDb && err == nil {
+		db.Exec("DELETE FROM logs")
 	}
 
 	return db, nil
