c6d9532778b8bf602d551dd118a0ebc934242408
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2021-09-02 00:32:34 -0300

    refactor: adicionado uso de start de worker e de calculo de tempo de processamento

--- main.go
+++ main.go
@@ -1,19 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"time"
 	"up-planilhas-go/core"
 )
 
 func main() {
-	done := make(chan bool)
-
+	start := time.Now()
 	db, err := core.NewDbConnection()
 	if err != nil {
 		log.Fatalf("Error on DB connection")
 	}
 
 	worker := core.NewWorker(db)
-	worker.Workers = 175
-	worker.WorkerRunner(done)
+	err = worker.Start()
+
+	elapsed := time.Since(start)
+	fmt.Printf("Tempo de processamento com %d workers: %s", worker.Workers, elapsed)
+
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
 }
